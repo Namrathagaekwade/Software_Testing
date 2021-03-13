a373a3a152b0452bce559868395e4c1551ddec75
jyo1304 <ananthaneni.jyothsna@gmail.com>
2021-03-13 22:06:05 +0100

    failed methods

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -2,20 +2,20 @@ class Calculator:
     def add(self,x, y):
         """This function adds two numbers"""
 
-        return x + y
+        return 0
 
     def subtract(self,x, y):
         """This function subtracts two numbers"""
 
-        return x - y
+        return 0
 
     def multiply(self,x, y):
         """This function multiplies two numbers"""
 
-        return x * y
+        return 0
 
     def divide(self,x, y):
         """This function divides two numbers"""
 
-        return x / y
+        return 0
 

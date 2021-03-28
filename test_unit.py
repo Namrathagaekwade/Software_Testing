f06b11572f8227366e56c86a494e011d20c72c5f
ishaks671 <ishaks671@gmail.com>
2021-03-28 13:25:25 +0200

    rectifying test cases

--- test_unit.py
+++ test_unit.py
@@ -8,15 +8,15 @@ class ArithTest (unittest.TestCase):
 
     def runTest (self):
 
-        self.assertEqual(self.cal.add(1, 1), 3, msg='1+2 = 3 failed')
+        self.assertEqual(self.cal.add(1, 2), 3, msg='1+2 = 3 failed')
         self.failIf(self.cal.divide(10, 2) == 3, msg='10/2 = 3 fail test failed')
         self.failUnlessEqual(self.cal.multiply(2, 2), 4, msg='2 * 2 failed')
-        self.assertTrue(self.cal.subtract(13, 5) == 7, msg='13 - 6 failed')
-        self.assertTrue(self.cal.multiply(5, 4) == 25, msg='5 * 5 failed')
-        self.assertEqual(self.cal.add(10, 10), 25, msg='10 + 15 = 25 failed')
-        self.assertEqual(self.cal.divide(50, 5), 5, msg='50 / 10 failed')
+        self.assertTrue(self.cal.subtract(13, 6) == 7, msg='13 - 6 failed')
+        self.assertTrue(self.cal.multiply(5, 5) == 25, msg='5 * 5 failed')
+        self.assertEqual(self.cal.add(10, 15), 25, msg='10 + 15 = 25 failed')
+        self.assertEqual(self.cal.divide(50, 10), 5, msg='50 / 10 failed')
         self.assertFalse(self.cal.subtract(20, 5) == 14, msg='20 - 5 = 14 fail test failed')
-        self.assertEqual(self.cal.divide(5,1), 1)
+        self.assertEqual(self.cal.divide(5,5), 1)
 
 def suite():
 
